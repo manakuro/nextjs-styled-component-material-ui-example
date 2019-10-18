8edd7f42ed4b791e283402dfb6ead0a7d35d5c21
manato.kuroda <a7108100@gmail.com>
2019-10-18 09:41:35 +0900

    feat: add styled-components and material-ui

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import NextApp from 'next/app'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  primary: 'green',
+}
+
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
+}
---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import NextDocument from 'next/document'
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
+
+export default class Document extends NextDocument {
+  static async getInitialProps (ctx) {
+    const styledComponentSheet = new StyledComponentSheets()
+    const materialUiSheets = new MaterialUiServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            styledComponentSheet.collectStyles(
+              materialUiSheets.collect(<App {...props} />),
+            ),
+        })
+
+      const initialProps = await NextDocument.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key="styles">
+            {initialProps.styles}
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      }
+    } finally {
+      styledComponentSheet.seal()
+    }
+  }
+}
